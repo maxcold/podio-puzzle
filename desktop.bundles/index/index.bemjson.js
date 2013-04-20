284960959fe5077cbee070446a431c3cddb85a65
maxcold <mxcold@gmail.com>
2013-04-20 14:39:20 +0400

    logo added

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,12 +1,12 @@
 ({
     block: 'b-page',
-    title: 'Title of the page',
+    title: 'Podio puzzle',
     favicon: 'https://d2cmuesa4snpwn.cloudfront.net/images/favicon.ico',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: '_index.js' },
-        { elem: 'meta', attrs: { name: 'description', content: 'Podio puzzle' }},
+        { elem: 'meta', attrs: { name: 'description', content: 'Podio front-end developer test' }}
     ],
     content:[
         {
