8d7aacb4f31d3320e00adab368cf6a1a702d1e63
maxcold <mxcold@gmail.com>
2013-04-20 14:10:36 +0400

    base grid and header added

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,32 +1,48 @@
 ({
     block: 'b-page',
     title: 'Title of the page',
-    favicon: '/favicon.ico',
+    favicon: 'https://d2cmuesa4snpwn.cloudfront.net/images/favicon.ico',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
-        { elem: 'css', url: '_index', ie: true },
         { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: '_index.js' },
-        { elem: 'meta', attrs: { name: 'description', content: '' }},
-        { elem: 'meta', attrs: { name: 'keywords', content: '' }}
+        { elem: 'meta', attrs: { name: 'description', content: 'Podio puzzle' }},
     ],
     content:[
         {
             block: 'header',
             content: [
-                'header content goes here'
+                {
+                    block: 'grid',
+                    mods: {
+                        layout: '20-80'
+                    },
+                    content: [
+                        {
+                            elem: 'col',
+                            mods: {
+                                type: 'left'
+                            },
+                            content: [
+                                {
+                                    block: 'logo'
+                                }
+                            ]
+                        },
+                        {
+                            elem: 'col',
+                            mods: {
+                                type: 'right'
+                            }
+                        }
+                    ]
+                }
             ]
         },
         {
             block: 'content',
             content: [
                 'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
             ]
         }
     ]
