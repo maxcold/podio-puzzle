c6a95a9cf8433d85b778777cbde2f02a71771abf
maxcold <mxcold@gmail.com>
2013-04-21 22:26:32 +0400

    add package builded for production

---
+++ production/desktop.bundles/index/index.deps.js
@@ -0,0 +1,194 @@
+exports.deps = [
+    {
+        "block": "i-jquery"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "inherit"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "identify"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "is-empty-object"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "debounce"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "observable"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "object"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "array"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "function"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "bemhtml"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "string"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "i-ua"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "core"
+    },
+    {
+        "block": "i-utils",
+        "elem": "normalize-css"
+    },
+    {
+        "block": "i-utils",
+        "elem": "throttle"
+    },
+    {
+        "block": "b-page"
+    },
+    {
+        "block": "b-page",
+        "elem": "css"
+    },
+    {
+        "block": "b-page",
+        "elem": "js"
+    },
+    {
+        "block": "space-switcher"
+    },
+    {
+        "block": "header"
+    },
+    {
+        "block": "grid"
+    },
+    {
+        "block": "grid",
+        "elem": "col"
+    },
+    {
+        "block": "grid",
+        "mod": "layout"
+    },
+    {
+        "block": "grid",
+        "mod": "layout",
+        "val": "20-80"
+    },
+    {
+        "block": "grid",
+        "mod": "type"
+    },
+    {
+        "block": "grid",
+        "mod": "type",
+        "val": "left"
+    },
+    {
+        "block": "grid",
+        "mod": "type",
+        "val": "right"
+    },
+    {
+        "block": "logo"
+    },
+    {
+        "block": "menu"
+    },
+    {
+        "block": "menu",
+        "elem": "link"
+    },
+    {
+        "block": "menu",
+        "elem": "item"
+    },
+    {
+        "block": "space-switcher",
+        "elem": "control"
+    },
+    {
+        "block": "space-switcher",
+        "elem": "arrow"
+    },
+    {
+        "block": "space-switcher",
+        "elem": "popup"
+    },
+    {
+        "block": "space-switcher-filter"
+    },
+    {
+        "block": "space-switcher-filter",
+        "elem": "item"
+    },
+    {
+        "block": "organization"
+    },
+    {
+        "block": "organization",
+        "elem": "create-space"
+    },
+    {
+        "block": "organization",
+        "elem": "icon"
+    },
+    {
+        "block": "organization",
+        "elem": "name"
+    },
+    {
+        "block": "list",
+        "elem": "item"
+    },
+    {
+        "block": "space-switcher-filter",
+        "elem": "input"
+    },
+    {
+        "block": "space-switcher-filter",
+        "elem": "spaces"
+    },
+    {
+        "block": "content"
+    }
+];
+exports.depsByTechs = {
+    "": {}
+};
